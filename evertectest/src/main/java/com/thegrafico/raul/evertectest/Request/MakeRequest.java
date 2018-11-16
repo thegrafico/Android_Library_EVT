d23ceb3c37c5e1b061907ff157ff74ed9c09f412
Raul Pichardo <raul022107@gmail.com>
2018-11-16 10:58:08 -0400

    This file make the request for all objects-Request that we created

--- evertectest/src/main/java/com/thegrafico/raul/evertectest/Request/MakeRequest.java
+++ evertectest/src/main/java/com/thegrafico/raul/evertectest/Request/MakeRequest.java
@@ -1,3 +1,11 @@
+/*--------------- MAKE REQUEST -----------------------
+
+   * Here are the logic to make the request for all the object request  tha we has created
+   * All of the object request make a simple request: 'POST'.
+
+  -----------------------------------------------------------
+*/
+
 package com.thegrafico.raul.evertectest.Request;
 
 import java.io.BufferedReader;
@@ -8,21 +16,27 @@ import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-
+//class name
 public class MakeRequest {
+
+    /*
+    *static methods to make request: receive 'dataToMakeRequest' and 'choose'
+    * dataToMakeRequest is a Json Object that we send here with data to make the request
+    * choose is an option to know what object was called and set the proper URL where data will be send.
+    */
 
     static public String getDataFromRequest(String dataToMakeRequest, int choose){
 
         //Object Connection
         HttpURLConnection urlConnection = null;
         try{
-
+            //set the URL with object URL (chooseURL is a function to choose the proper URL)
             URL url = new URL(chooseURL(choose));
 
-            //Object Connection
+            //Object Connection and open the connection
             urlConnection = (HttpURLConnection) url.openConnection();
 
-            //SetUp
+            //SetUp the request
             urlConnection.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
             urlConnection.setRequestProperty("Accept", "application/json");
             urlConnection.setDoOutput(true);
@@ -31,31 +45,32 @@ public class MakeRequest {
             //Open Connection
             urlConnection.connect();
 
-            //Send Data setup
+            //Here que are setup the data(The request JSOn Object we gonna send
             OutputStream os = urlConnection.getOutputStream();
             OutputStreamWriter osw = new OutputStreamWriter(os, "UTF-8");
 
-            //sending data
+            //sending request (data)
             osw.write(dataToMakeRequest);
 
             //clean/close sending data
             osw.flush();
             osw.close();
 
-            //To read Data
-
+            //Here we get the response from the web service  or where we are making the request
             BufferedReader streamReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream(), "UTF-8"));
+
+            //StringBuilding to append string in a clean a fast way
             StringBuilder responseStrBuilder = new StringBuilder();
             String inputStr;
 
-            //while there is data to read
+            //while there is data to read, append the response into my stringBuilder
             while ((inputStr = streamReader.readLine()) != null)
                 responseStrBuilder.append(inputStr);
 
             //close de streamReader
             streamReader.close();
 
-            //returns the json object
+            //returns the Response in json object format
             return responseStrBuilder.toString();
 
             //error handle
@@ -64,7 +79,7 @@ public class MakeRequest {
         } catch (IOException e) {
             e.printStackTrace();
         }finally {
-
+            //close the urlConnection
             if (urlConnection != null){
                 urlConnection.disconnect();
             }
@@ -74,8 +89,10 @@ public class MakeRequest {
         return null;
     }
 
+
+    //private function to choose what URL call
     private static String chooseURL(int choose){
-        String URL = " ";
+        String URL;
 
         switch (choose){
             case 1: //Process Debit Transaction
@@ -100,6 +117,8 @@ public class MakeRequest {
             case 7: //Process Transaction Search Transaction
                 URL = "http://192.168.43.118:2000/processTransactionSearch/";
                 break;
+            default:
+                URL = null;
         }
 
         return URL;
