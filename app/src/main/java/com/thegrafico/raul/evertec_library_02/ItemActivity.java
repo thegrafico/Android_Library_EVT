c73e1733e62848f002e799fbef309271f0d82d9f
Raul Pichardo <raul022107@gmail.com>
2018-11-30 13:45:50 -0400

    Layout

--- app/src/main/java/com/thegrafico/raul/evertec_library_02/ItemActivity.java
+++ app/src/main/java/com/thegrafico/raul/evertec_library_02/ItemActivity.java
@@ -31,7 +31,7 @@ public class ItemActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_items);
 
         //get the intent info
         Intent infoAcc = getIntent();
